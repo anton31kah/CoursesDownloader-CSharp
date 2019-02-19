8b737dc375a4c9843dcbd0c1e7d665affd9d4e91
anton31kah <anton.kahwaji@gmail.com>
2019-02-19 23:27:54 +0100

    added branch, and todo

--- CoursesDownloader/CoursesDownloader.Downloader/TasksTodo.cs
+++ CoursesDownloader/CoursesDownloader.Downloader/TasksTodo.cs
@@ -2,7 +2,16 @@
 {
     internal static class TasksTodo
     {
-        // TO/DO 00 CHECK TO/DO TASK LIST (CTRL+\, T) (AND SORT BY DESCRIPTION)
+        // TODO 00 CHECK TODO TASK LIST (CTRL+\, T) (AND SORT BY DESCRIPTION)
         
+        // TODO 01 delete second line in creds file all the time (check for it on start, whenever possible) // try not storing it all
+
+        // TODO 02 logging
+
+        // TODO 03 login get timeout
+
+        // TODO 04 multiple downloads
+
+        // TODO 05 allow download stop (on close delete files)
     }
 }
